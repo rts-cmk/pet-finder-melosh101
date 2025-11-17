112468f1c67361b13b9418058ba9d008f7527556
Milas Holsting <git@milasholsting.dk>
2025-11-17 14:20:00 +0100

    feat(frontend): add pets page

--- apps/frontend/src/pages/pets.tsx
+++ apps/frontend/src/pages/pets.tsx
@@ -1,21 +1,37 @@
 import { useLoaderData } from "react-router";
-import {HiOutlineBell} from "react-icons/hi";
+import { HiOutlineBell, HiChevronDown, HiLocationMarker } from "react-icons/hi";
+import type { Pet } from "backend/src/schema";
 
 export default function petsPage() {
-  const { pets } = useLoaderData()
+  const { pets, user } = useLoaderData() as {
+    pets: Pet[]
+    user: {
+      id: number;
+      name: string;
+      image: string;
+      location: string;
+    }
+  }
   console.log(pets)
   return (
     <>
-    <main>
+    <header className="pets-header">
+      <img src={user.image} alt={user.name} className="profile-picture"/>
+      <span className="user-location"><HiLocationMarker /> {user.location} <HiChevronDown /></span>
+      <div className="notifications-button">
+        <HiOutlineBell/>
+      </div>
+    </header>
+      <main>
 
-      <h1></h1>
-      <ul>
-        {pets.map((pet: { id: number; breed: string }) => (
-          <li key={pet.id}>{pet.breed}</li>
-        ))}
-      </ul>
-    </main>
+        <h1></h1>
+        <ul>
+          {pets.map((pet) => (
+            <li key={pet.id}>{pet.breed}</li>
+          ))}
+        </ul>
+      </main>
     </>
-    
+
   )
 }
